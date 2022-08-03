5abe152e88eb4c3b5125e74d8a4e42debc8b5c3b
vincesesto <vince_sesto@mail.com>
2022-08-03 13:48:55 +1200

    Update test_python_script.py

--- test_python_script.py
+++ test_python_script.py
@@ -10,3 +10,8 @@ def test_passwd_file(host):
     assert passwd.user == "root"
     assert passwd.group == "root"
     assert passwd.mode == 0o644
+
+def test_python_is_installed(host):
+    python = host.package("python")
+    assert python.is_installed
+    assert python.version.startswith("2")
