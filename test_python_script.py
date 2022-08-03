6915eb1f9003fe1e1b9fc9d7c83958b66206d325
vincesesto <vince_sesto@mail.com>
2022-08-03 13:11:39 +1200

    Update test_python_script.py

--- test_python_script.py
+++ test_python_script.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 
-print("Testing With Python")
+import testinfra
+
+print("Testing Python")
+
+def test_passwd_file(host):
+    passwd = host.file("/etc/passwrd")
+    assert passwd.containes("root")
+
+host = testinfra.get_host("localhost")
+
+test_passwd_file(host)
